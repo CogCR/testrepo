beddf88a0930d3d102bfe470c9be03133f9544c2
midjuly <midjuly@users.noreply.github.com>
2018-09-27 15:41:25 -0700

    update with sec center

--- cognosys-oms2/scripts/Automated-IaaS-Backup.ps1
+++ cognosys-oms2/scripts/Automated-IaaS-Backup.ps1
@@ -84,6 +84,130 @@ Catch {
         Write-Error -Message $ErrorMessage `
                     -ErrorAction Stop
       }
+      
+      
+      
+      
+      # new code for security
+      
+      
+      
+      Try {
+       #region update PS
+
+Install-Module -Name PowerShellGet -Force
+Install-Module -Name AzureRM.Security -AllowPrerelease
+Install-Module -Name  AzureRm.OperationalInsights
+#endregion update PS
+
+#region List all ASC PowerShell commands
+Get-Command -Module AzureRm.Security
+#endregion
+
+#region Azure Microsoft.Security ResourceProvider registration
+#Verify registration
+Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Security | Select-Object ProviderNamespace, Locations, RegistrationState
+
+#Register the Microsoft.Security resource provider
+Register-AzureRmResourceProvider -ProviderNamespace Microsoft.Security
+#endregion
+
+#region Assign ASC Azure Policy
+#Assign the ASC Azure Policies to a subscription
+$mySub = Get-AzureRmSubscription -SubscriptionID $subscriptionId #-SubscriptionName "<mySubscriptionName>"
+$subscription = "/subscriptions/$mySub"
+$policySetDefinition = Get-AzureRmPolicySetDefinition | Where-Object {$_.Properties.DisplayName -eq "[Preview]: Enable Monitoring in Azure Security Center"}
+New-AzureRmPolicyAssignment -PolicySetDefinition $policySetDefinition -Name $OMSWorkspaceName"assigname" -Scope $subscription -PolicyParameter "{}"
+
+#Assign the ASC Azure Policies to a resource group
+New-AzureRmResourceGroup -Name $OMSResourceGroupName -Location  $Location
+$resourceGroup = Get-AzureRmResourceGroup -Name $OMSResourceGroupName
+$policySetDefinition = Get-AzureRmPolicySetDefinition | Where-Object {$_.Properties.DisplayName -eq "[Preview]: Enable Monitoring in Azure Security Center"}
+New-AzureRmPolicyAssignment -PolicySetDefinition $policySetDefinition -Name $OMSWorkspaceName"assigname" -Scope $resourceGroup.ResourceId -PolicyParameter "{}"
+#endregion
+
+#region GET Autoprovision settings for subscriptions
+#Get Autoprovision setting for the current scope
+Get-AzureRmSecurityAutoProvisioningSetting
+
+#Get the Autoprovision setting for all Azure subscriptions 
+Get-AzureRmContext -ListAvailable -PipelineVariable myAzureSubs | Set-AzureRmContext | ForEach-Object{
+    Write-Output $myAzureSubs
+    Get-AzureRmSecurityAutoProvisioningSetting | Select-Object AutoProvision
+    "-"*100
+}
+
+
+# get subs from all and enable for all
+
+$subscriptions = Get-AzureRmSubscription
+
+foreach($subNameFromFile in $subscriptions.Id){
+
+try{
+#  Write-Output "/subscriptions/$subNameFromFile"
+  $subsidforscope= "/subscriptions/$subNameFromFile"
+
+$workspaceObj = Get-AzureRmOperationalInsightsWorkspace -Name $OMSWorkspaceName -ResourceGroupName $OMSResourceGroupName
+Set-AzureRmSecurityWorkspaceSetting -Name default -WorkspaceId $workspaceObj.ResourceId -Scope $subsidforscope
+
+
+
+
+}catch
+    {
+    $ErrorMessage = 'subscription loop error'
+            $ErrorMessage += " `n"
+            $ErrorMessage += 'Error: '
+            $ErrorMessage += $_
+            Write-Error -Message $ErrorMessage `
+                        -ErrorAction ignore
+    }
+}
+# get subs from all and enable for all
+
+
+
+#endregion
+
+#region SET AutoProvision settings
+#Set AutoProvision to ON for the current scope
+Set-AzureRmSecurityAutoProvisioningSetting -Name "default" -EnableAutoProvision
+
+#Set AutoProvision to ON for all subscriptions
+Get-AzureRmContext -ListAvailable -PipelineVariable myAzureSubs | Set-AzureRmContext | ForEach-Object{
+    Set-AzureRmSecurityAutoProvisioningSetting -Name "default" -EnableAutoProvision
+}
+
+
+#endregion
+
+#region Azure Security Pricing
+#Get current pricing tier
+Get-AzureRmSecurityPricing | Select-Object Name, PricingTier
+
+#Set Azure Security Center pricing tier for the default scope, use either "Standard" or "Free"
+Set-AzureRmSecurityPricing -Name default -PricingTier "Standard"
+
+#region Security Alerts
+#Tip: you can filter out fields of interest by using Select-Object
+Get-AzureRmSecurityAlert
+Get-AzureRmSecurityAlert | Select-Object AlertDisplayName, CompromisedEntity, Description
+#endregion
+
+
+
+
+    }
+
+Catch {
+        $ErrorMessage = 'Security Center errors'
+        $ErrorMessage += "`n"
+        $ErrorMessage += 'Error: '
+        $ErrorMessage += $_
+        Write-Error -Message $ErrorMessage `
+                    -ErrorAction ignore
+      }
 
 
 
